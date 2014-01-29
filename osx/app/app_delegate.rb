4cdd3934151ddffa59e8b8caae9c0ab8d8f59cea
Scott Barber <scottb@ScottBookPro-2.local>
2014-01-29 13:55:10 -0700

    dPanda is alive

---
+++ osx/app/app_delegate.rb
@@ -0,0 +1,42 @@
+class AppDelegate
+  attr_accessor :status_menu
+
+  def applicationDidFinishLaunching(notification)
+
+    @app_name = NSBundle.mainBundle.infoDictionary['CFBundleDisplayName']
+
+    @status_menu = NSMenu.new
+
+    @status_item = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength).init
+    @status_item.setMenu(@status_menu)
+    @status_item.setHighlightMode(true)
+    @status_item.setTitle(@app_name)
+
+    #@status_menu.addItem createMenuItem("About #{@app_name}", 'orderFrontStandardAboutPanel:')
+    @status_menu.addItem createMenuItem("Test Audio", 'testAudioAction')
+    @status_menu.addItem createMenuItem("Quit", 'terminate:')
+
+    # Do the firebase stuff here
+    firebase = Firebase.new(FIREBASE_URL)
+    firebase.auth(FIREBASE_AUTH)
+
+    firebase.on(:changed) { |snapshot|
+      playAudio(snapshot.value['sound'])
+    }
+  end
+
+  def createMenuItem(name, action)
+    NSMenuItem.alloc.initWithTitle(name, action: action, keyEquivalent: '')
+  end
+
+  def playAudio(clip_name)
+    ap clip_name
+    # Cringeworthy code below
+    `curl -s http://cp.insops.net/panda/#{clip_name} -o /tmp/dPanda_#{clip_name} && afplay -t 5 /tmp/dPanda_#{clip_name}`
+  end
+
+  def testAudioAction
+    playAudio('headshot')
+  end
+
+end
